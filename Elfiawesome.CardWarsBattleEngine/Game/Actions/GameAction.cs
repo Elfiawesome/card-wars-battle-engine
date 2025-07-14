b1b2749aaed9e09f73f721a241abcf659275ab1b
Elfiawesome23 <elfiawesome23@gmail.com>
2025-07-14 12:21:48 +0800

    Remove unnecessary comments in SetupGame and add GameAction abstract class

---
+++ Elfiawesome.CardWarsBattleEngine/Game/Actions/GameAction.cs
@@ -0,0 +1,6 @@
+namespace Elfiawesome.CardWarsBattleEngine.Game.Actions;
+
+public abstract class GameAction
+{
+	public abstract void Execute(CardWarsBattleEngine engine);
+}
--- Elfiawesome.CardWarsBattleEngine/Game/Intents/SetupGame.cs
+++ Elfiawesome.CardWarsBattleEngine/Game/Intents/SetupGame.cs
@@ -6,9 +6,6 @@ public class SetupGame : GameIntent
 	{
 		engine.isGameStarted = true;
 
-		// I want everything that happen in this game "saved" so that we can replay it again
-		// So when the game for example initializes, we need to know what we did so that we can pack that data and sent off to the clients on what to do
-
 		// Initialize the battlefield for each players
 		foreach (var _p in engine._players)
 		{
