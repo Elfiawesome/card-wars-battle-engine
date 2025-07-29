ebf0148a87191094948b8896e43fc34d68a4d4ee
Elfiawesome23 <elfiawesome23@gmail.com>
2025-07-29 18:14:31 +0800

    Added UnitSlot and Battlefield relationship

--- CardWars.BattleEngine/Core/States/Battlefield.cs
+++ CardWars.BattleEngine/Core/States/Battlefield.cs
@@ -3,6 +3,7 @@ namespace CardWars.BattleEngine.Core.States;
 public class Battlefield(BattlefieldId id)
 {
 	public readonly BattlefieldId Id = id;
+	public List<UnitSlotId> UnitSlots { get; } = new();
 }
 
 public readonly record struct BattlefieldId(long Value);
--- CardWars.BattleEngine/Core/States/UnitSlot.cs
+++ CardWars.BattleEngine/Core/States/UnitSlot.cs
@@ -4,6 +4,9 @@ public class UnitSlot(UnitSlotId id)
 {
 	public readonly UnitSlotId Id = id;
 	public UnitId? HoldingUnit { get; set; }
+	public UnitSlotPos Position { get; set; } = new(0, 0);
 }
 
 public readonly record struct UnitSlotId(long Value);
+public readonly record struct UnitSlotPos(long X, long Y);
+
