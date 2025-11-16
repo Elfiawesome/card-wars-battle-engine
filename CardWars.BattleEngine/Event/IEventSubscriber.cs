5b81862e1e1f8839d4f8288d25976715b6948a67
Elfiawesome23 <elfiawesome23@gmail.com>
2025-11-16 20:14:05 +0800

    Added battle engine context to event subscriber

--- CardWars.BattleEngine/Event/IEventSubscriber.cs
+++ CardWars.BattleEngine/Event/IEventSubscriber.cs
@@ -1,8 +1,10 @@
+using CardWars.BattleEngine.Resolver;
+
 namespace CardWars.BattleEngine.Event;
 
 public interface IEventSubscriber<TEvent>
 	where TEvent : IEvent
 {
 	public int Priority => 0;
-	public void Handle(TEvent evnt);
+	public void Handle(BattleEngine engine, TEvent evnt);
 }
