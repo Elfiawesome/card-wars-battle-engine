af78ebdbbee0a81cfabc72bdfac18b7f7d07f8f3
elfiawesome23@gmail.com <elfiawesome23@gmail.com>
2026-02-03 22:54:23 +0800

    Update project configuration and add game session structure
    - Upgrade Godot SDK version to 4.5.2-rc.1
    - Change project reference from Vanilla to Server
    - Remove unused NewScript and associated files
    - Create GameSession class with basic structure
    - Add battle scene and game session scene files

---
+++ src/CardWars.Client/scenes/game_session/GameSession.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+namespace CardWars.Client;
+
+public partial class GameSession : Node
+{
+	public override void _Ready()
+	{
+		
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
