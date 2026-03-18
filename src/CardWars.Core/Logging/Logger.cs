12bc30c09fb67bb149a3dabd961309dfc274517d
elfiawesome23@gmail.com <elfiawesome23@gmail.com>
2026-03-18 10:35:02 +0800

    Renamed to Logger.cs and expose api for logging

--- src/CardWars.Core/Logging/Logger.cs
+++ src/CardWars.Core/Logging/Logger.cs
@@ -2,6 +2,7 @@ namespace CardWars.Core.Logging;
 
 public static class Logger
 {
+	public static Action<ConsoleColor, string, string>? OnLog { get; set; }
 	public static void Info(string msg) => Print(msg, "INFO", ConsoleColor.White);
 
 	public static void Debug(string msg) => Print(msg, "DEBUG", ConsoleColor.DarkGray);
@@ -11,11 +12,12 @@ public static class Logger
 	public static void Error(string msg) => Print(msg, "ERROR", ConsoleColor.Red);
 
 	public static void Custom(string msg, string level = "---", ConsoleColor color = ConsoleColor.Cyan) => Print(msg, level, color);
-	
+
 	private static void Print(string msg, string level, ConsoleColor color = ConsoleColor.White)
 	{
 		Console.ForegroundColor = color;
 		Console.WriteLine($"[{level}]: {msg}");
 		Console.ResetColor();
+		OnLog?.Invoke(color, level, msg);
 	}
 }
