73b3cf6fcfd96467c67694eece5663ba22eb142d
elfiawesome23@gmail.com <elfiawesome23@gmail.com>
2026-06-14 23:19:30 +0800

    Add WorldId and Data properties to WorldInstance for session management

--- src/CardWars.Server.Vanilla/Session/WorldInstance.cs
+++ src/CardWars.Server.Vanilla/Session/WorldInstance.cs
@@ -1,5 +1,6 @@
 using CardWars.Core.Data;
 using CardWars.Core.Network.Packet;
+using CardWars.Core.Registry;
 using CardWars.Server.Session;
 
 namespace CardWars.Server.Vanilla.Session;
@@ -8,6 +9,9 @@ namespace CardWars.Server.Vanilla.Session;
 public class WorldInstance : IServerInstance
 {
 	[DataTag] public Guid InstanceId { get; set; }
+	[DataTag] public ResourceId WorldId { get; set; }
+	[DataTag] public CompoundTag Data { get; set; } = new(); // session related data
+	public CompoundTag TemplateData { get; set; } = new(); // For reference only
 
 	public void AddPlayer(PlayerSession player) { }
 	public void RemovePlayer(PlayerSession player) { }
